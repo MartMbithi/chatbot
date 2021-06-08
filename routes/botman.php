aca3997e09c1141c54937f9a0d1130464bde73b7
MartMbithi <martdevelopers254@gmail.com>
2021-06-08 15:03:18 +0300

    Initiated Conversion

--- routes/botman.php
+++ routes/botman.php
@@ -1,9 +1,15 @@
 <?php
+
 use App\Http\Controllers\BotManController;
 
 $botman = resolve('botman');
-
+/* Lets Initiate First Contact */
 $botman->hears('Hi', function ($bot) {
     $bot->reply('Hello!');
 });
-$botman->hears('Start conversation', BotManController::class.'@startConversation');
+
+$botman->hears('Howdy', function ($bot) {
+    $bot->reply('Im feeling great, what about you?');
+});
+
+$botman->hears('Start conversation', BotManController::class . '@startConversation');
