4f4cd1ad906cfd38a6cf4166d4a1f3aa0e8c2f56
MartMbithi <martdevelopers254@gmail.com>
2021-06-08 14:52:04 +0300

    Initiated Contact

--- resources/views/tinker.blade.php
+++ resources/views/tinker.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>BotMan Studio</title>
+    <title>ChatBot</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet" type="text/css">
@@ -29,20 +30,22 @@
         .content {
             text-align: center;
         }
-                
+
         #app {
             position: absolute;
             bottom: 10px;
         }
     </style>
 </head>
-<body>
-<div class="container">
-    <div class="content" id="app">
-        <botman-tinker api-endpoint="/botman"></botman-tinker>
-    </div>
-</div>
 
-<script src="/js/app.js"></script>
+<body>
+    <div class="container">
+        <div class="content" id="app">
+            <botman-tinker api-endpoint="/botman"></botman-tinker>
+        </div>
+    </div>
+
+    <script src="/js/app.js"></script>
 </body>
+
 </html>
